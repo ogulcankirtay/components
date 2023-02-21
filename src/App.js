04fbf0fcd5537dd52fd64574ce0770b340dd2e05
Ogulcan <ogulcankirtay8@gmail.com>
2023-02-21 20:38:09 +0300

    jsx_rules_learned

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/header';
+
 function App(){
   return(
-    <div>
+    <>
       <Header></Header>
-    </div>
+      <p className='my_paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates?</p>
+      <br/>
+      <label htmlFor='my_input'>
+        Name  
+        <input id='my_input' placeholder='E-mail'></input>
+      </label>
+    </>
   );
 }
 export default App;
