4cd73e0f7f5070f5edbb28a6662ee72a32c01648
Ogulcan <ogulcankirtay8@gmail.com>
2023-02-21 22:10:34 +0300

    conditional_render_complated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
 import Header from './components/header';
 
-const name="Oğulcan"
-const sureName="Kırtay"
-
+const name="Oğulcan";
+const sureName="Kırtay";
+const isLoggedIn=false;
 function App(){
   return(
     <>
-      <Header></Header>
-      <p className='my_paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates?</p>
-      <br/>
-      <h1>Rendered Data by {name} {sureName} </h1>
-      <h2>{`benim adım ${name} ${sureName}`}</h2>
-      <label htmlFor='my_input'>
-        Name  
-        <input id='my_input' placeholder='E-mail'></input>
-      </label>
+     <h1>{isLoggedIn &&  `Welcome`}</h1>
+     <h1>{isLoggedIn ? `Welcome with ?` : 'Log in with ?'}</h1>
+     <h1>{!isLoggedIn &&  `Log in`}</h1>
+
     </>
   );
 }
