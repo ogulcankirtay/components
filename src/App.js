d0eacbe936db30629edb491d1f2a45b52b61476e
Ogulcan <ogulcankirtay8@gmail.com>
2023-02-21 22:03:19 +0300

    render_completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/header';
 
+const name="Oğulcan"
+const sureName="Kırtay"
+
 function App(){
   return(
     <>
       <Header></Header>
       <p className='my_paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae eveniet nisi debitis delectus deleniti aperiam illum autem eligendi voluptates?</p>
       <br/>
+      <h1>Rendered Data by {name} {sureName} </h1>
+      <h2>{`benim adım ${name} ${sureName}`}</h2>
       <label htmlFor='my_input'>
         Name  
         <input id='my_input' placeholder='E-mail'></input>
